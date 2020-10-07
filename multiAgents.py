741605828e91a87e7bc3ea4859ec20a9f4550f5f
muskaangoyal <31122612+muskaangoyal@users.noreply.github.com>
2020-10-08 05:10:15 +0530

    Added q1

--- multiAgents.py
+++ multiAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -74,12 +74,13 @@ class ReflexAgent(Agent):
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
         "*** YOUR CODE HERE ***"
+        newGhost = successorGameState.getGhostPositions()
         foodDist = newFood.asList()
         score = successorGameState.getScore()
         for dist in foodDist:
             newfoodDist = util.manhattanDistance(newPos, dist)
             score += 1/newfoodDist
-       
+
 
         #if ghost too near, then don't go in that Direction
         for ghostDist in newGhost:
